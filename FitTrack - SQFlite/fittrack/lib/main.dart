90eed5148314fd50d36c938a2f4a53f13e0b7c99
TomTruyenPXL <11900339@student.pxl.be>
2020-12-31 20:06:45 +0100

    Added orientation, provider & updated theme

--- FitTrack - SQFlite/fittrack/lib/main.dart
+++ FitTrack - SQFlite/fittrack/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 import 'package:fittrack/screens/Wrapper.dart';
+import 'package:fittrack/models/exercises/ExerciseFilter.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,25 +12,37 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FitTrack',
-      theme: ThemeData(
-        fontFamily: 'OpenSans',
-        iconTheme: IconThemeData(
-          color: Color.fromRGBO(50, 100, 200, 1),
-        ),
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          unselectedItemColor: Colors.grey,
-          selectedItemColor: Color.fromRGBO(50, 100, 200, 1),
-          unselectedIconTheme: IconThemeData(
-            size: 24.0,
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ExerciseFilter>(create: (_) => ExerciseFilter()),
+      ],
+      child: MaterialApp(
+        title: 'FitTrack',
+        theme: ThemeData(
+          fontFamily: 'OpenSans',
+          accentColor: Color.fromRGBO(50, 100, 200, 1),
+          iconTheme: IconThemeData(
+            color: Color.fromRGBO(50, 100, 200, 1),
           ),
-          selectedIconTheme: IconThemeData(
-            size: 26.0,
+          buttonColor: Color.fromRGBO(50, 100, 200, 1),
+          bottomNavigationBarTheme: BottomNavigationBarThemeData(
+            unselectedItemColor: Colors.grey,
+            selectedItemColor: Color.fromRGBO(50, 100, 200, 1),
+            unselectedIconTheme: IconThemeData(
+              size: 24.0,
+            ),
+            selectedIconTheme: IconThemeData(
+              size: 26.0,
+            ),
           ),
         ),
+        home: Wrapper(),
       ),
-      home: Wrapper(),
     );
   }
 }
