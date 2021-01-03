74aca72fb04899668ebc5c7f8eb955713543e589
Tom Truyen <tom.truyen@gmail.com>
2021-01-03 15:53:58 +0100

    added delete popup

---
+++ FitTrack - SQFlite/fittrack/lib/screens/exercises/popups/DeleteExercisePopup.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+import 'package:fittrack/shared/ErrorPopup.dart';
+import 'package:fittrack/shared/Functions.dart';
+import 'package:fittrack/shared/Globals.dart' as globals;
+
+Future<void> showPopupDeleteExercise(
+  BuildContext context,
+  int id,
+  String name,
+  Function updateUserExercises,
+) async {
+  await showDialog(
+    context: context,
+    barrierDismissible: true,
+    builder: (BuildContext context) {
+      return StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          return Center(
+            child: Container(
+              constraints: BoxConstraints(
+                maxWidth: 250.0,
+                maxHeight: MediaQuery.of(context).size.height * 0.80,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                color: Colors.grey[100],
+                border: Border.all(
+                  width: 0,
+                ),
+              ),
+              padding: EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 4.0),
+              child: SingleChildScrollView(
+                child: Theme(
+                  data: Theme.of(context).copyWith(
+                    dividerColor: Color.fromRGBO(70, 70, 70, 1),
+                    unselectedWidgetColor: Color.fromRGBO(
+                      200,
+                      200,
+                      200,
+                      1,
+                    ),
+                  ),
+                  child: Material(
+                    color: Colors.grey[100],
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            'Are you sure?',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            'Are you sure you want to delete \'$name\' from your exercises?',
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              FlatButton(
+                                child: Text(
+                                  'Cancel',
+                                  style: TextStyle(
+                                    fontSize: 13.0,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                onPressed: () {
+                                  tryPopContext(context);
+                                },
+                              ),
+                              FlatButton(
+                                child: Text(
+                                  'OK',
+                                  style: TextStyle(
+                                    fontSize: 13.0,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                onPressed: () async {
+                                  dynamic result =
+                                      globals.sqlDatabase.deleteExercise(id);
+
+                                  if (result == null) {
+                                    tryPopContext(context);
+
+                                    showPopupError(
+                                      context,
+                                      'Deleting exercise failed',
+                                      'Something went wrong deleting \'$name\'. Please try again.',
+                                    );
+                                  } else {
+                                    await updateUserExercises();
+                                    tryPopContext(context);
+                                  }
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      );
+    },
+  );
+}
