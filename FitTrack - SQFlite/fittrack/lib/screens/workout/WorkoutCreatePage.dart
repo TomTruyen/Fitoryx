181727592041a283b3afd79af71c98de35adacef
Tom Truyen <tom.truyen@gmail.com>
2021-01-04 18:29:56 +0100

    added template for createworkoutpage

---
+++ FitTrack - SQFlite/fittrack/lib/screens/workout/WorkoutCreatePage.dart
@@ -0,0 +1,42 @@
+import 'package:fittrack/shared/Functions.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class WorkoutCreatePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: <Widget>[
+          SliverAppBar(
+            backgroundColor: Colors.grey[50],
+            floating: true,
+            pinned: true,
+            title: Text(
+              'Create Workout',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            leading: IconButton(
+              icon: Icon(
+                Icons.close,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                tryPopContext(context);
+              },
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Text(
+              'Make the create workout page here, for each exercise use \'Cards\' widget',
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
