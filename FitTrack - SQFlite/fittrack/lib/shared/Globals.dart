0e47b313964af25e873de5760c2881be198a99e3
Tom Truyen <tom.truyen@gmail.com>
2021-01-26 18:34:15 +0100

    added pageenum

--- FitTrack - SQFlite/fittrack/lib/shared/Globals.dart
+++ FitTrack - SQFlite/fittrack/lib/shared/Globals.dart
@@ -1,3 +1,12 @@
 import 'package:fittrack/services/SQLDatabase.dart';
 
 SQLDatabase sqlDatabase;
+
+enum PageEnum {
+  // globals.PageEnum.workout.index to get the index value for the 'changePage' function
+  profile,
+  history,
+  workout,
+  exercises,
+  food,
+}
