6ff8aab58bd44995ee76f7bbcf78abc12180a7aa
TomTruyenPXL <11900339@student.pxl.be>
2021-01-28 19:29:02 +0100

    added SettingsPage

---
+++ FitTrack - SQFlite/fittrack/lib/screens/settings/SettingsPage.dart
@@ -0,0 +1,36 @@
+import 'package:fittrack/shared/Functions.dart';
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: <Widget>[
+          SliverAppBar(
+            backgroundColor: Colors.grey[50],
+            floating: true,
+            pinned: true,
+            title: Text(
+              'Settings',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            leading: IconButton(
+              icon: Icon(
+                Icons.close,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                tryPopContext(context);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
