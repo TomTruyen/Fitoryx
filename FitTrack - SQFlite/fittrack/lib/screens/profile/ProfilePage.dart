d3fde388e4c4fb36ab7e9f92cad6cc4a4abbd85b
TomTruyenPXL <11900339@student.pxl.be>
2021-01-28 19:28:53 +0100

    Added template design

--- FitTrack - SQFlite/fittrack/lib/screens/profile/ProfilePage.dart
+++ FitTrack - SQFlite/fittrack/lib/screens/profile/ProfilePage.dart
@@ -1,10 +1,45 @@
+import 'package:fittrack/screens/settings/SettingsPage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return Scaffold(
+      body: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: <Widget>[
+          SliverAppBar(
+            backgroundColor: Colors.grey[50],
+            floating: true,
+            pinned: true,
+            title: Text(
+              'Profile',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(
+                  Icons.settings_outlined,
+                  color: Colors.black,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    CupertinoPageRoute(
+                      fullscreenDialog: true,
+                      builder: (context) => SettingsPage(),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
