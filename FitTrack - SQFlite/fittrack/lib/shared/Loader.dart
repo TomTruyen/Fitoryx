75ec131b4ad06ba027b1ccbc56aec835d59e49da
Tom Truyen <tom.truyen@gmail.com>
2021-01-28 20:00:32 +0100

    added text to loader

--- FitTrack - SQFlite/fittrack/lib/shared/Loader.dart
+++ FitTrack - SQFlite/fittrack/lib/shared/Loader.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 
 class Loader extends StatelessWidget {
+  final String text;
+
+  Loader({this.text});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.grey[50],
-      child: Center(
-        child: CircularProgressIndicator(
-          backgroundColor: Colors.grey[200],
-          valueColor: AlwaysStoppedAnimation<Color>(Colors.grey[500]),
-          strokeWidth: 2.0,
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        color: Colors.grey[50],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            CircularProgressIndicator(
+              backgroundColor: Colors.grey[200],
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.grey[500]),
+              strokeWidth: 2.0,
+            ),
+            if (text != null)
+              Container(
+                margin: EdgeInsets.only(top: 16.0),
+                child: Text(text),
+              ),
+          ],
         ),
       ),
     );
