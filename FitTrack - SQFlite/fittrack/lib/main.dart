c45efbbf489bb4325acadc9a4c3525dec9d067ec
TomTruyenPXL <11900339@student.pxl.be>
2021-01-28 19:32:29 +0100

    added loader + todo

--- FitTrack - SQFlite/fittrack/lib/main.dart
+++ FitTrack - SQFlite/fittrack/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fittrack/shared/Loader.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
@@ -24,6 +25,8 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
 
+    // Doe deze stappen in een isolate zodat het van de main thread wordt gehaald
+    // Geef loader widget een optie voor text mee te geven (en geef in de futurebuilder dan de tekst 'Loading...' mee)
     globals.sqlDatabase = new SQLDatabase();
     _database = globals.sqlDatabase.setupDatabase().then((value) => value);
   }
@@ -69,7 +72,7 @@ class _MyAppState extends State<MyApp> {
           future: _database,
           builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
             if (!snapshot.hasData) {
-              return Container(color: Colors.white);
+              return Loader();
             } else {
               return Wrapper();
             }
