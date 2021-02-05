74f2994a4a10db0d59491ffc177447d7e8ae2305
Tom Truyen <tom.truyen@gmail.com>
2021-02-05 19:33:34 +0100

    added workout delete popup

---
+++ FitTrack - SQFlite/fittrack/lib/screens/workout/popups/DeleteWorkoutPopup.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+import 'package:fittrack/shared/ErrorPopup.dart';
+import 'package:fittrack/shared/Functions.dart';
+import 'package:fittrack/shared/Globals.dart' as globals;
+
+Future<void> showPopupDeleteWorkout(
+  BuildContext context,
+  int id,
+  Function updateWorkouts,
+) async {
+  await showDialog(
+    context: context,
+    barrierDismissible: true,
+    builder: (BuildContext context) {
+      return StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          return Center(
+            child: Container(
+              width: 250.0,
+              height: 250.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                color: Colors.grey[100],
+                border: Border.all(
+                  width: 0,
+                ),
+              ),
+              padding: EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 4.0),
+              child: Theme(
+                data: Theme.of(context).copyWith(
+                  dividerColor: Color.fromRGBO(70, 70, 70, 1),
+                  unselectedWidgetColor: Color.fromRGBO(
+                    200,
+                    200,
+                    200,
+                    1,
+                  ),
+                ),
+                child: Material(
+                  color: Colors.grey[50],
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            'Are you sure?',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: SingleChildScrollView(
+                          child: Container(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              'Are you sure you want to delete this workout?',
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              FlatButton(
+                                child: Text(
+                                  'Cancel',
+                                  style: TextStyle(
+                                    fontSize: 13.0,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                onPressed: () {
+                                  tryPopContext(context);
+                                },
+                              ),
+                              FlatButton(
+                                child: Text(
+                                  'OK',
+                                  style: TextStyle(
+                                    fontSize: 13.0,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                onPressed: () async {
+                                  dynamic result = await globals.sqlDatabase
+                                      .deleteWorkout(id);
+
+                                  if (result != null) {
+                                    await updateWorkouts();
+
+                                    tryPopContext(context);
+                                  } else {
+                                    tryPopContext(context);
+
+                                    showPopupError(
+                                      context,
+                                      'Failed to delete',
+                                      'Something went wrong deleting this workout. Please try again.',
+                                    );
+                                  }
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      );
+    },
+  );
+}
