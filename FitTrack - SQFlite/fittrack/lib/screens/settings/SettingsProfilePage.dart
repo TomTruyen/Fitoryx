611f1ab3c8ba4ebfd4d28be4b252cad71143cd1d
Tom Truyen <tom.truyen@gmail.com>
2021-03-21 17:00:10 +0100

    Updated design + implemented new settings

--- FitTrack - SQFlite/fittrack/lib/screens/settings/SettingsProfilePage.dart
+++ FitTrack - SQFlite/fittrack/lib/screens/settings/SettingsProfilePage.dart
@@ -1,7 +1,9 @@
 import 'package:fittrack/functions/Functions.dart';
 import 'package:fittrack/models/settings/Settings.dart';
+import 'package:fittrack/screens/settings/popups/dateofbirth/DateOfBirthPopup.dart';
 import 'package:fittrack/shared/ErrorPopup.dart';
 import 'package:fittrack/shared/Globals.dart' as globals;
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SettingsProfilePage extends StatefulWidget {
@@ -17,7 +19,14 @@ class SettingsProfilePage extends StatefulWidget {
 class _SettingsProfilePageState extends State<SettingsProfilePage> {
   double weight;
   String weightUnit;
+
+  double height;
+  String heightUnit;
+
   double bodyFat;
+
+  String gender;
+  DateTime dateOfBirth;
 
   @override
   void initState() {
@@ -26,7 +35,17 @@ class _SettingsProfilePageState extends State<SettingsProfilePage> {
     setState(() {
       weight = widget.settings?.userWeight[0]?.weight ?? 0.0;
       weightUnit = widget.settings?.weightUnit ?? "kg";
+      height = widget.settings?.height ?? 0.0;
+      heightUnit = widget.settings?.heightUnit ?? 'cm';
       bodyFat = widget.settings.bodyFat[0].percentage ?? 0.0;
+      gender = widget.settings?.gender ?? "male";
+      dateOfBirth = widget.settings?.dateOfBirth;
+    });
+  }
+
+  void updateDateOfBirth(DateTime dob) {
+    setState(() {
+      dateOfBirth = dob;
     });
   }
 
@@ -64,6 +83,10 @@ class _SettingsProfilePageState extends State<SettingsProfilePage> {
                 ),
                 onPressed: () async {
                   Settings newSettings = widget.settings.clone();
+                  newSettings.height = height;
+                  newSettings.dateOfBirth = dateOfBirth;
+                  newSettings.gender = gender;
+
                   bool isInsertUserWeight = newSettings.tryAddUserWeight(
                     weight,
                     weightUnit,
@@ -82,6 +105,7 @@ class _SettingsProfilePageState extends State<SettingsProfilePage> {
 
                   if (userWeightInputResult != null &&
                       bodyFatInputResult != null) {
+                    await globals.sqlDatabase.updateSettings(newSettings);
                     await widget.updateSettings(newSettings);
                     tryPopContext(context);
                   } else {
@@ -103,6 +127,18 @@ class _SettingsProfilePageState extends State<SettingsProfilePage> {
                 mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
+                  Flexible(
+                    child: Container(
+                      margin: EdgeInsets.only(left: 12.0),
+                      child: Text(
+                        'Personal Info',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 12.0),
                   Flexible(
                     child: Row(
                       children: <Widget>[
@@ -139,6 +175,37 @@ class _SettingsProfilePageState extends State<SettingsProfilePage> {
                         SizedBox(width: 12.0),
                         Flexible(
                           child: TextFormField(
+                            initialValue: height != null
+                                ? tryConvertDoubleToInt(height).toString()
+                                : '0',
+                            keyboardType: TextInputType.numberWithOptions(
+                              decimal: true,
+                            ),
+                            textInputAction: TextInputAction.next,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.all(12.0),
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              floatingLabelBehavior:
+                                  FloatingLabelBehavior.always,
+                              fillColor: Colors.grey[300],
+                              filled: true,
+                              hintText: '0',
+                              hintStyle: TextStyle(color: Colors.black54),
+                              suffixText: heightUnit.toUpperCase(),
+                            ),
+                            onChanged: (String value) {
+                              if (value == "") value = "0";
+
+                              height = double.parse(value);
+                            },
+                          ),
+                        ),
+                        SizedBox(width: 12.0),
+                        Flexible(
+                          child: TextFormField(
                             initialValue: bodyFat != null
                                 ? tryConvertDoubleToInt(bodyFat).toString()
                                 : '0',
@@ -166,10 +233,88 @@ class _SettingsProfilePageState extends State<SettingsProfilePage> {
                               bodyFat = double.parse(value);
                             },
                           ),
-                        )
+                        ),
                       ],
                     ),
                   ),
+                  SizedBox(height: 24.0),
+                  Flexible(
+                    child: Container(
+                      margin: EdgeInsets.only(left: 12.0),
+                      child: Text(
+                        'Date of Birth',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    contentPadding: EdgeInsets.only(left: 12.0),
+                    title: Text(
+                      dateOfBirth != null
+                          ? convertDateTimeToString(dateOfBirth)
+                          : "No date of birth set",
+                    ),
+                    onTap: () async {
+                      clearFocus(context);
+
+                      await showPopupDateOfBirth(
+                          context, updateDateOfBirth, widget.settings);
+                    },
+                  ),
+                  SizedBox(height: 24.0),
+                  Flexible(
+                    child: Container(
+                      margin: EdgeInsets.only(left: 12.0),
+                      child: Text(
+                        'Gender',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                      child: RadioListTile(
+                    activeColor: Colors.blue[700],
+                    selected: gender == 'male',
+                    contentPadding: EdgeInsets.zero,
+                    dense: true,
+                    title: Text(
+                      'Male',
+                      style: Theme.of(context).textTheme.bodyText2,
+                    ),
+                    groupValue: gender,
+                    value: 'male',
+                    onChanged: (String value) {
+                      clearFocus(context);
+
+                      setState(() {
+                        gender = value;
+                      });
+                    },
+                  )),
+                  Flexible(
+                      child: RadioListTile(
+                    activeColor: Colors.blue[700],
+                    selected: gender == 'female',
+                    contentPadding: EdgeInsets.zero,
+                    dense: true,
+                    title: Text(
+                      'Female',
+                      style: Theme.of(context).textTheme.bodyText2,
+                    ),
+                    groupValue: gender,
+                    value: 'female',
+                    onChanged: (String value) {
+                      clearFocus(context);
+
+                      setState(() {
+                        gender = value;
+                      });
+                    },
+                  )),
                 ],
               ),
             ),
