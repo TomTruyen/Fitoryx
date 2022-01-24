d8028fa841c1486c9e2f1095e5033ed1b6361383
TomTruyen <tom.truyen@gmail.com>
2022-01-24 15:52:56 +0100

    Implement basic firebase

--- Fitoryx - Firebase (NEW)/fitoryx/lib/main.dart
+++ Fitoryx - Firebase (NEW)/fitoryx/lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+// Firebase Options has to be generated (README.MD)
+import 'firebase_options.dart';
+
+void main() async {
+  // Firebase Setup
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
