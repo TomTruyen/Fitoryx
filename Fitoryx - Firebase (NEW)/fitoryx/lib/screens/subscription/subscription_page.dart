055aeffac6e515953d0c6f29d098871df4fb9a9d
TomTruyen <tom.truyen@gmail.com>
2022-02-12 14:55:24 +0100

    Add subscription page design

--- Fitoryx - Firebase (NEW)/fitoryx/lib/screens/subscription/subscription_page.dart
+++ Fitoryx - Firebase (NEW)/fitoryx/lib/screens/subscription/subscription_page.dart
@@ -1,4 +1,6 @@
+import 'package:fitoryx/widgets/gradient_button.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SubscriptionPage extends StatelessWidget {
   SubscriptionPage({Key? key}) : super(key: key);
@@ -23,30 +25,112 @@ class SubscriptionPage extends StatelessWidget {
           controller: _controller,
           children: <Widget>[
             _overview(),
-            _information(),
+            _pricing(),
           ],
         ),
       ),
     );
   }
 
-  Column _overview() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: const <Widget>[
-        Text("HI"),
-      ],
+  Container _overview() {
+    return Container(
+      margin: const EdgeInsets.all(16),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 4),
+                  child: Text(
+                    'Fitoryx Pro',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 4),
+                  child: Text(
+                    'Get Fitoryx Pro and unlock all features to get better insights on your progress',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black54,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                _feature("Create unlimited workouts"),
+                _feature("Create unlimited exercises"),
+                _feature("Charts, analytics and insights"),
+              ],
+            ),
+          ),
+          _pricingButton(),
+        ],
+      ),
     );
   }
 
-  Column _information() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: const <Widget>[
-        Text("HI 2"),
-      ],
+  Container _feature(String text) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Row(
+        children: <Widget>[
+          const Icon(Icons.check_circle, size: 20),
+          const SizedBox(width: 5),
+          Text(text)
+        ],
+      ),
+    );
+  }
+
+  Container _pricing() {
+    return Container(
+      margin: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text("TODO BUILD PAGE"),
+          //
+          Center(
+            child: TextButton(
+              child: Text(
+                'MANAGE SUBSCRIPTIONS',
+                style: TextStyle(
+                  color: Colors.blue[600],
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              onPressed: () async {
+                const String subscriptions =
+                    "http://play.google.com/store/account/subscriptions";
+
+                if (await canLaunch(subscriptions)) {
+                  launch(subscriptions);
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  GradientButton _pricingButton() {
+    return GradientButton(
+      text: "Pricing",
+      onPressed: () {
+        _controller.animateToPage(
+          1,
+          curve: Curves.easeInOut,
+          duration: const Duration(milliseconds: 500),
+        );
+      },
     );
   }
 }
