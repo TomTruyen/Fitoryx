ef9c19aa945fb05025e3ebe0e3cd2970e29929cb
TomTruyen <tom.truyen@gmail.com>
2022-02-18 13:49:50 +0100

    Add GoogleButton tests

---
+++ Fitoryx - Firebase (NEW)/fitoryx/test/lib/widgets/google_button_test.dart
@@ -0,0 +1,48 @@
+import 'package:fitoryx/widgets/google_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('GoogleButton', () {
+    testWidgets('has ElevatedButton', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: GoogleButton(
+              onPressed: () => {},
+            ),
+          ),
+        ),
+      );
+
+      final elevatedButtonFinder = find.byType(ElevatedButton);
+
+      expect(elevatedButtonFinder, findsOneWidget);
+    });
+
+    testWidgets(
+      'has google image asset',
+      (WidgetTester tester) async {
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: GoogleButton(
+                onPressed: () => {},
+              ),
+            ),
+          ),
+        );
+
+        final imageFinder = find.byType(Image);
+
+        expect(imageFinder, findsOneWidget);
+
+        Image image = tester.firstWidget(imageFinder);
+        expect(
+          (image.image as AssetImage).assetName,
+          "assets/images/google.png",
+        );
+      },
+    );
+  });
+}
