f6049e4c054bee7e086dacad0cad6a5600fb7d5e
TomTruyen <tom.truyen@gmail.com>
2022-02-18 15:24:13 +0100

    Add ExerciseRow tests

---
+++ Fitoryx - Firebase (NEW)/fitoryx/test/lib/widgets/exercise_row_test.dart
@@ -0,0 +1,81 @@
+import 'package:fitoryx/widgets/exercise_row.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Loader', () {
+    testWidgets(
+      'displays empty string when sets en name is empty',
+      (WidgetTester tester) async {
+        await tester.pumpWidget(
+          const MaterialApp(
+            home: Scaffold(
+              body: ExerciseRow(
+                sets: "",
+                name: "",
+                equipment: "",
+              ),
+            ),
+          ),
+        );
+
+        final textFinder = find.text("");
+
+        expect(textFinder, findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      'displays "sets x name" when equipment is empty',
+      (WidgetTester tester) async {
+        const sets = "10";
+        const name = "ExerciseName";
+
+        const expected = "10 x ExerciseName";
+
+        await tester.pumpWidget(
+          const MaterialApp(
+            home: Scaffold(
+              body: ExerciseRow(
+                sets: sets,
+                name: name,
+                equipment: "",
+              ),
+            ),
+          ),
+        );
+
+        final textFinder = find.text(expected);
+
+        expect(textFinder, findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      'displays "sets x name (equipment)" when all values not empty',
+      (WidgetTester tester) async {
+        const sets = "10";
+        const name = "ExerciseName";
+        const equipment = "Equipment";
+
+        const expected = "10 x ExerciseName (Equipment)";
+
+        await tester.pumpWidget(
+          const MaterialApp(
+            home: Scaffold(
+              body: ExerciseRow(
+                sets: sets,
+                name: name,
+                equipment: equipment,
+              ),
+            ),
+          ),
+        );
+
+        final textFinder = find.text(expected);
+
+        expect(textFinder, findsOneWidget);
+      },
+    );
+  });
+}
