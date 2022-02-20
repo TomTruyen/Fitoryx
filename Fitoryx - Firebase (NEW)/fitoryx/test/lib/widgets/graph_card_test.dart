383b0bc4b7255d25d1fd9e6802dc53ff6d6d8851
TomTruyen <tom.truyen@gmail.com>
2022-02-20 11:50:27 +0100

    Add GraphCard tests

---
+++ Fitoryx - Firebase (NEW)/fitoryx/test/lib/widgets/graph_card_test.dart
@@ -0,0 +1,33 @@
+import 'package:fitoryx/widgets/graph_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('GraphCard', () {
+    late GraphCard card;
+
+    setUp(() {
+      card = const GraphCard(
+        title: "GraphTitle",
+        graph: SizedBox(child: Text("Graph Widget")),
+        popup: null,
+      );
+    });
+    testWidgets(
+      'has Card',
+      (WidgetTester tester) async {
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: card,
+            ),
+          ),
+        );
+
+        final cardFinder = find.byType(Card);
+
+        expect(cardFinder, findsOneWidget);
+      },
+    );
+  });
+}
