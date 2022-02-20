69e0ada348522f5004dcdc095eb1dec1cce8e818
TomTruyen <tom.truyen@gmail.com>
2022-02-20 12:17:33 +0100

    Add ManageSubscriptionButton tests

---
+++ Fitoryx - Firebase (NEW)/fitoryx/test/lib/widgets/manage_subscriptions_button_test.dart
@@ -0,0 +1,48 @@
+import 'package:fitoryx/widgets/manage_subscription_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('ManageSubscriptionsButton', () {
+    testWidgets(
+      'has TextButton',
+      (WidgetTester tester) async {
+        ManageSubscriptionsButton button = const ManageSubscriptionsButton();
+
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: button,
+            ),
+          ),
+        );
+
+        final textButtonFinder = find.byType(TextButton);
+
+        expect(textButtonFinder, findsOneWidget);
+
+        await tester.tap(textButtonFinder);
+
+      },
+    );
+
+    testWidgets(
+      'has "MANAGE SUBSCRIPTIONS" text',
+      (WidgetTester tester) async {
+        ManageSubscriptionsButton button = const ManageSubscriptionsButton();
+
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: button,
+            ),
+          ),
+        );
+
+        final textFinder = find.text("MANAGE SUBSCRIPTIONS");
+
+        expect(textFinder, findsOneWidget);
+      },
+    );
+  });
+}
